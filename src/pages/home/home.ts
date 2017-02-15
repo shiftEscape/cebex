d0a461706e0501f7c9f7708e26b670b01c8160b8
Alvin James Bellero <ajames.bellero@gmail.com>
2017-02-15 20:34:17 +0800

    :relieved: Embedded Google Maps in home screen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -8,8 +9,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('map') mapElement;
+  map: any;
+
   constructor(public navCtrl: NavController) {
     
+  }
+
+  ionViewDidLoad() {
+    this.initMap();
+  }
+
+  initMap () {
+    let latLng = new google.maps.LatLng(10.315699, 123.885437);
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
   }
 
 }
