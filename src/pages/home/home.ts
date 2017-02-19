6a050b373601e83dfd178460993e3ccf87632158
Alvin James Bellero <ajames.bellero@gmail.com>
2017-02-19 23:38:27 +0800

    Added custom polyline embed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Config } from '../../config';
 
 declare var google;
 
@@ -11,6 +12,7 @@ export class HomePage {
 
   @ViewChild('map') mapElement;
   map: any;
+  paths = [];
 
   constructor(public navCtrl: NavController) {
     
@@ -20,14 +22,34 @@ export class HomePage {
     this.initMap();
   }
 
+  // Generate LatLng Google format
+  mapLatLng (routesArray): any {
+    var routePath = []; 
+    for(var i in routesArray) {
+      routePath.push(new google.maps.LatLng(routesArray[i][0], routesArray[i][1]));
+    } return routePath;
+  }
+
   initMap () {
-    let latLng = new google.maps.LatLng(10.310197, 123.893671);
+
+    // Create Google Map instance
+    let latLng = new google.maps.LatLng(Config.focusMapCoords.lat, Config.focusMapCoords.lng);
+
     let mapOptions = {
       center: latLng,
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    // Display all available routes from config
+    for(var routeCode in Config.routes) {
+      this.paths[routeCode] = new google.maps.Polyline({
+        path: this.mapLatLng(Config.routes[routeCode].coords), strokeColor: Config.routes[routeCode].strokeColor, strokeOpacity: 0.5, strokeWeight: 5
+      });
+      this.paths[routeCode].setOptions({ map: this.map });
+    }
   }
 
 }
